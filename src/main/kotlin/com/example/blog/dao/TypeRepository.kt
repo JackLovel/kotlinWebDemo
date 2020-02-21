38f49b1e9a76b5b34c71a2a1069614d8f5bd64c9
JackLovel <gogkatsu@outlook.com>
2020-02-22 07:25:53 +0800

    update

--- src/main/kotlin/com/example/blog/dao/TypeRepository.kt
+++ src/main/kotlin/com/example/blog/dao/TypeRepository.kt
@@ -3,6 +3,7 @@ package com.example.blog.dao
 
 import com.example.blog.model.Type
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface TypeRepository : JpaRepository<Type, Long> {
     fun findByName(name : String) : Type
